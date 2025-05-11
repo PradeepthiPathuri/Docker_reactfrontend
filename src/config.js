b7dc363fe57cbaf16b6aedb791029727aa7e5d16
Avinash Dola <2300030169@kluniversity.in>
2025-05-11 06:24:28 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = 
 {
-    "url":"http://springbackend-production-9c63.up.railway.app:8080"
+    "url":"https://springbackend-production-9c63.up.railway.app:8080"
 }
 
 export default config
